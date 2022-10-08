2a4115f3bed1f2ca8d1e0ff438f4d68451b56ccb
albsel <alban_selimi@yahoo.com>
2022-10-08 14:18:58 +0200

    final-verison

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "729e9880-60f2-4815-aaed-6f9d9a8d4276";
+export const MODAL_CLOSE_SEC = 2.5;
